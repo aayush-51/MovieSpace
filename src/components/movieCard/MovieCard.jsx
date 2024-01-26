774d31ac698c35746f3313ab02ca9bcbe719124e
aayush-51 <157700341+aayush-51@users.noreply.github.com>
2024-01-26 21:39:07 +0530

    MovieCard.jsx

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-function MovieCard({ allMovieData, loading }) {
+function MovieCard({ allMovieData, loading, searchPerformed }) {
   return (
     <div>
-      {loading ?
+      {loading ? (
         <div className='flex justify-center '>
-          <img className=' w-16 py-20' src="https://i.gifer.com/ZZ5H.gif" alt="" />
+          <img className='w-16 py-20' src="https://i.gifer.com/ZZ5H.gif" alt="" />
         </div>
-        :
+      ) : searchPerformed && allMovieData.length === 0 ? (
+        <p className='text-white text-center mt-4'>No movies found.</p>
+      ) : (
         <div className='flex flex-wrap px-4 lg:px-10 '>
           {allMovieData.map((item, index) => {
-            const { Title, Year, Poster } = item
+            const { Title, Year, Poster } = item;
             return (
               <div key={index} className="p-2 md:w-1/4 w-full">
                 <div className="bg-[#40407a] p-3 rounded-2xl shadow-lg hover:-translate-y-1 border-2 border-gray-200">
@@ -19,12 +21,12 @@ function MovieCard({ allMovieData, loading }) {
                   <h2 className='text-lg text-white mb-2'>Year : {Year}</h2>
                 </div>
               </div>
-            )
+            );
           })}
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
