befcc14e5b0936d45e5adecae2dc993187953a21
aayush-51 <157700341+aayush-51@users.noreply.github.com>
2024-01-26 21:38:34 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from './components/navbar/Navbar'
-import MovieCard from './components/movieCard/MovieCard'
-import SearchBar from './components/searchBar/SearchBar'
+import React, { useEffect, useState } from 'react';
+import Navbar from './components/navbar/Navbar';
+import MovieCard from './components/movieCard/MovieCard';
+import SearchBar from './components/searchBar/SearchBar';
 
 function App() {
   const [allMovieData, setAllMovieData] = useState([]);
   const [searchMovie, setSearchMovie] = useState('');
-  
   const [loading, setLoading] = useState(false);
-    const fetchMovieData = async () => {
+  const [searchPerformed, setSearchPerformed] = useState(false);
+
+  const fetchMovieData = async () => {
     try {
-        setLoading(true);
-        const res = await fetch(`https://omdbapi.com/?s=${searchMovie}&apikey=a1de9591`);
-        const data = await res.json();
+      setLoading(true);
+      const res = await fetch(`https://omdbapi.com/?s=${searchMovie}&apikey=a1de9591`);
+      const data = await res.json();
+
+      if (data.Response === 'True') {
         setAllMovieData(data.Search);
-        setLoading(false)
+      } else {
+        setAllMovieData([]);
+      }
+
+      setLoading(false);
+      setSearchPerformed(true); // Set searchPerformed to true after the search
     } catch (error) {
-        console.log(error)
-        setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
-}
+  };
 
   return (
     <div>
       <Navbar />
-      <div className="bg ">
+      <div className="bg">
         <SearchBar searchMovie={searchMovie} setSearchMovie={setSearchMovie} fetchMovieData={fetchMovieData} />
-        <MovieCard allMovieData={allMovieData} loading={loading} />
+        <MovieCard allMovieData={allMovieData} loading={loading} searchPerformed={searchPerformed} />
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
